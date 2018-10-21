3d8fedfa43fb042905441327a1f3c984f7d3773b
ALIENWARE-TONY\antho <anthony_chouan@hotmail.com>
2018-10-21 18:38:17 -0400

    Fix package.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  coveoUrl: "https://cloudplatform.coveo.com/rest/search?access_token=058c85fd-3c79-42a3-9236-b83d35588103",
+  resultsPerPage: 12
 };
