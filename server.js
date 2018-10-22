cc41499360dd0a2f47903fcf3f61f28567fc914d
ALIENWARE-TONY\antho <anthony_chouan@hotmail.com>
2018-10-21 21:01:17 -0400

    Fix package.json

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/CoveoAnthony-App>'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/CoveoAnthony-App>/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/CoveoAnthony-App/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
